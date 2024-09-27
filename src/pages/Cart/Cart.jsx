6b98305c666807f361b8be063a463fa2ddb614cf
bekturb <bekkgboy2@gmail.com>
2024-09-27 10:56:39 +0600

    deleted all logs

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,7 +3,6 @@ import Helmet from "../../layout/Helmet";
 import SingleCart from "../../components/SingleCart/SingleCart";
 
 const Cart = () => {
-    console.log(Beka);
     
     return (
         <Helmet title="Cart">
